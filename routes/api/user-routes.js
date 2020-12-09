85ef5e3d4746d562aedf532a37ce4703634b677c
MarynaPR <pryadkamaryna@gmail.com>
2020-12-08 22:40:42 -0600

    comments added

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,6 +1,5 @@
-//Create, read, update, and delete
 const router = require('express').Router();
-const { User, Post, Vote } = require('../../models');
+const { User, Post, Comment, Vote } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
@@ -24,6 +23,14 @@ router.get('/:id', (req, res) => {
             {
                 model: Post,
                 attributes: ['id', 'title', 'post_url', 'created_at']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at'],
+                include: {
+                    model: Post,
+                    attributes: ['title']
+                }
             },
             {
                 model: Post,
