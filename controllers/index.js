0b434d6f839701cefbd114e304d809f6131f20ae
MarynaPR <pryadkamaryna@gmail.com>
2020-12-11 12:50:09 -0600

    view and public folders added with corresponding files and code

--- controllers/index.js
+++ controllers/index.js
@@ -2,12 +2,19 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
 
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
 });
+// const dashboardRoutes = require('./dashboard-routes.js');
+
+router.use('/', homeRoutes);
+// router.use('/dashboard', dashboardRoutes);
+
+
 
 module.exports = router;
 
